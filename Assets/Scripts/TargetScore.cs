44b7b7126eb822d376a7962044d765911ad62eed
Claire Domeny <crmdomeny2310@gmail.com>
2021-05-03 16:47:24 +1000

    Janky script
    the target script isn't working properly. Need to rewrite it. That'll happen another time.

--- Assets/Scripts/TargetScore.cs
+++ Assets/Scripts/TargetScore.cs
@@ -5,41 +5,28 @@ using UnityEngine;
 public class TargetScore : MonoBehaviour
 {
     [SerializeField] private BoxCollider boxCollider;
-    [SerializeField] private CapsuleCollider outer, middle, inner, bullseye;
-    int where = 0;
+    [SerializeField] private MeshCollider c2, c4, c6, c8, c10;
+    
+    static int score = 0;
+    int tempScore = 0;
+    
     private void OnTriggerEnter(Collider _other)
     {
-        if (_other == outer) where = 1;
-        if (_other == middle) where = 2;
-        if (_other == inner) where = 3;
-        if (_other == bullseye) where = 4;
+        if (_other == c10) tempScore = 10;
+        else if (_other == c8) tempScore = 8;
+        else if (_other == c6) tempScore = 6;
+        else if (_other == c4) tempScore = 4;
+        else if (_other == c2) tempScore = 2;
     }
 
     private void OnCollisionEnter(Collision _collision)
     {
         if (_collision.collider == boxCollider)
         {
-            switch (where)
-            {
-                case 0:
-                    Debug.Log("Missed.");
-                    break;
-                case 1:
-                    Debug.Log("Hit outter ring.");
-                    break;
-                case 2:
-                    Debug.Log("Hit middle ring.");
-                    break;
-                case 3:
-                    Debug.Log("Hit inner ring.");
-                    break;
-                case 4:
-                    Debug.Log("Bullseye!");
-                    break;
-                default:
-                    Debug.Log("Missed?");
-                    break;
-            }
+            score += tempScore;
+            print(score.ToString());
+            tempScore = 0;
         }
     }
 }
+// wait no. this script is the wrong way around. crud
