cd69145a015b0e02cad8ca1bfa6c686890a3f125
Claire Domeny <crmdomeny2310@gmail.com>
2021-05-11 16:33:28 +1000

    Revamped target code.

--- Assets/Scripts/TargetScore.cs
+++ Assets/Scripts/TargetScore.cs
@@ -1,32 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TargetScore : MonoBehaviour
 {
-    [SerializeField] private BoxCollider boxCollider;
+    [SerializeField] private MeshCollider trigger;
     [SerializeField] private MeshCollider c2, c4, c6, c8, c10;
     
     static int score = 0;
     int tempScore = 0;
-    
-    private void OnTriggerEnter(Collider _other)
-    {
-        if (_other == c10) tempScore = 10;
-        else if (_other == c8) tempScore = 8;
-        else if (_other == c6) tempScore = 6;
-        else if (_other == c4) tempScore = 4;
-        else if (_other == c2) tempScore = 2;
-    }
+
+    private void Start() => Debug.LogWarning($"Change the {gameObject.name}'s OnCollisionEnter function to appropriately find arrow.");
 
     private void OnCollisionEnter(Collision _collision)
     {
-        if (_collision.collider == boxCollider)
+        if (_collision.gameObject.CompareTag("Arrow"))
         {
+            var hit = _collision.contacts[0].point;
+            var dis = Vector3.Distance(transform.position, hit);
+            print("Distance = " + dis);
+
+            tempScore++;
             score += tempScore;
-            print(score.ToString());
+            print("Score: " + score.ToString());
             tempScore = 0;
         }
     }
 }
-// wait no. this script is the wrong way around. crud
