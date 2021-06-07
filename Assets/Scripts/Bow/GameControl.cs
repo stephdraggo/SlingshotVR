fba3985471f1eac33b9f9fa3e912642550222ff9
stephdraggo <steph.valais@gmx.com>
2021-06-07 10:56:47 +1000

    menus basically functional

--- Assets/Scripts/Bow/GameControl.cs
+++ Assets/Scripts/Bow/GameControl.cs
@@ -6,17 +6,23 @@ namespace Bow
     {
         private static GameControl instance;
 
+        [SerializeField] private bool inVR = true;
+
+
         //serialized for testing
         [SerializeField] private int score;
+
         private void Awake()
         {
             if (!instance) instance = this;
             else Destroy(this);
+            Wakaba.VR.VrUtils.SetVREnabled(inVR);
         }
 
         private void Start()
         {
             score = 0;
+
         }
 
         public static void AddScore(int _amount)
