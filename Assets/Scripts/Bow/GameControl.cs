47bc1e47aed724856cbb47f44f1b5755df0c0c50
Finn <varvells@gmail.com>
2021-06-15 10:26:22 +1000

    Push

--- Assets/Scripts/Bow/GameControl.cs
+++ Assets/Scripts/Bow/GameControl.cs
@@ -1,18 +1,33 @@
+using System;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 using Wakaba.VR;
 using TMPro;
+using UnityEngine.UI;
 
 namespace Bow
 {
+    public enum GameMode
+    {
+        Free,
+        Frenzy,
+        Strategic
+    }
     public class GameControl : MonoBehaviour
     {
-        private static GameControl instance;
+        public static GameControl instance;
         [SerializeField] private bool useVR;
+        [SerializeField] private float timer;
+        private float timerCount;
+        
+        public static GameMode gameMode = GameMode.Free;
 
-        private int score;
+        public static int score;
         [SerializeField] private TMP_Text scoreText;
-        private void Awake()
+        [SerializeField] private TMP_Text timerText;
+        [SerializeField] private Text endScoreText;
+        [SerializeField] private GameObject endPanel;
+         private void Awake()
         {
             if (useVR) VrUtils.SetVREnabled(true);
             if (!instance) instance = this;
@@ -23,12 +38,44 @@ namespace Bow
         {
             score = 0;
             if (scoreText) scoreText.text = "Score: 0";
+            if (gameMode == GameMode.Frenzy)
+            {
+                timerCount = timer;
+            }
+            else if (gameMode == GameMode.Free) timerText.gameObject.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (gameMode != GameMode.Frenzy) return;
+            if (timerCount > 0)
+            {
+                timerText.text = "Time: " + (int)timerCount;
+                timerCount -= Time.deltaTime;
+            }
+            else
+            {
+               EndGame();
+            }
+        }
+
+        public void SetArrowText(string _text)
+        {
+            timerText.text = _text;
+        }
+
+        [SerializeField] private Pause pause;
+
+        public void EndGame()
+        {
+            pause.PauseGame(true);
+            endScoreText.text = "Score: " + score;
         }
 
         public static void AddScore(int _amount)
         {
-            instance.score += _amount;
-            if (instance.scoreText) instance.scoreText.text = "Score: " + instance.score;
+            score += _amount;
+            if (instance.scoreText) instance.scoreText.text = "Score: " + score;
         }
     }
 }
